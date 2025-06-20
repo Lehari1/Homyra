111f465f743d18bb5a4e8364ba86f2e61c21c332
lerii <lerii.5052@gmail.com>
2025-06-20 05:40:20 +0530

    Fix: added SSL config for Render PostgreSQL

--- server/db.js
+++ server/db.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+    ssl: {
+    rejectUnauthorized: false, // ✅ Required for Render-hosted PostgreSQL
+  },
 });
 
 module.exports = pool;
